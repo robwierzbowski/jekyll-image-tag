84935e95cbd0f3412a86a935c5b24d53f42d18e0
James Musgrave <mail@jamesmusgrave.com>
2014-08-25 18:23:54 +0100

    Update image_tag.rb
    Fix for animated gifs

--- image_tag.rb
+++ image_tag.rb
@@ -106,6 +106,7 @@ module Jekyll
       end
 
       image = MiniMagick::Image.open(image_source_path)
+      image.coalesce
       digest = Digest::MD5.hexdigest(image.to_blob).slice!(0..5)
 
       image_dir = File.dirname(instance[:src])
@@ -159,6 +160,7 @@ module Jekyll
           i.resize "#{gen_width}x#{gen_height}^"
           i.gravity "center"
           i.crop "#{gen_width}x#{gen_height}+0+0"
+          i.layers "Optimize"
         end
 
         image.write gen_dest_file
